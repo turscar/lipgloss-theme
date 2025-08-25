38fd47a1a99e15c249132a649a16667ffdd5569f
Steve Atkins <steve@wordtothewise.com>
2025-08-25 09:06:40 +0100

    Ready to go.

---
+++ style.go
@@ -0,0 +1,188 @@
+package theme
+
+import (
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+)
+
+// Defines the PurpleClay palette of color shades
+var (
+	S950 = lipgloss.Color("#07000f")
+	S900 = lipgloss.Color("#130027")
+	S800 = lipgloss.Color("#1e003f")
+	S700 = lipgloss.Color("#280057")
+	S600 = lipgloss.Color("#33006f")
+	S500 = lipgloss.Color("#3d0087")
+	S400 = lipgloss.Color("#48009f")
+	S300 = lipgloss.Color("#5712ab")
+	S200 = lipgloss.Color("#6725b7")
+	S100 = lipgloss.Color("#906ccf")
+	S50  = lipgloss.Color("#a980db")
+)
+
+// Defines the PurpleClay palette for accent color shades
+var (
+	Green900 = lipgloss.Color("#087331")
+	Green700 = lipgloss.Color("#15803d")
+	Amber900 = lipgloss.Color("#bf4102")
+	Amber700 = lipgloss.Color("#db4b02")
+	Red900   = lipgloss.Color("#ab0513")
+	Red700   = lipgloss.Color("#db0f20")
+)
+
+var (
+	// A defines a PurpleClay themed hyperlink that supports both light and dark terminals
+	A = lipgloss.NewStyle().
+		Bold(true).
+		Underline(true).
+		Foreground(lipgloss.AdaptiveColor{
+			Light: string(S400),
+			Dark:  string(S100),
+		})
+
+	h = lipgloss.NewStyle().Padding(0, 1).Bold(true).Foreground(lipgloss.Color("#ffffff"))
+
+	// H1 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H1 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S50),
+			Dark:  string(S200),
+		})
+
+	// H2 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H2 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S100),
+			Dark:  string(S300),
+		})
+
+	// H3 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H3 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S200),
+			Dark:  string(S400),
+		})
+
+	// H4 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H4 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S300),
+			Dark:  string(S500),
+		})
+
+	// H5 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H5 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S400),
+			Dark:  string(S600),
+		})
+
+	// H6 defines a PurpleClay themed header that is ranked on importance from
+	// H1 (most) to H6 (least). Supports both light and dark terminals
+	H6 = h.
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S500),
+			Dark:  string(S700),
+		})
+
+	// Mark defines a PurpleClay themed text decoration for highlighting text. Supports
+	// both light and dark terminals
+	Mark = lipgloss.NewStyle().
+		Padding(0, 1).
+		Background(lipgloss.AdaptiveColor{
+			Light: string(S50),
+			Dark:  string(S700),
+		})
+
+	// I defines a PurpleClay themed hyperlink that supports both light and dark terminals
+	I = lipgloss.NewStyle().Italic(true)
+
+	// U defines a PurpleClay themed underline text decoration
+	U = lipgloss.NewStyle().Underline(true)
+
+	// B defines a PurpleClay themed bold text decoration
+	B = lipgloss.NewStyle().Bold(true)
+
+	// S defines a PurpleClay themed strikethrough text decoration
+	S = lipgloss.NewStyle().Strikethrough(true)
+
+	// Tick defines a PurpleClay themed glyph ✓ that supports both light and dark terminals
+	Tick = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{
+			Light: string(Green900),
+			Dark:  string(Green700),
+		}).
+		Render("✓")
+
+	// Cross defines a PurpleClay themed glyph x that supports both light and dark terminals
+	Cross = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{
+			Light: string(Red900),
+			Dark:  string(Red700),
+		}).
+		Render("✕")
+
+	// Bang defines a PurpleClay themed glyph ! that supports both light and dark terminals
+	Bang = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{
+			Light: string(Amber900),
+			Dark:  string(Amber700),
+		}).
+		Render("!")
+
+	// Logging defines a PurpleClay themed [logging] style that supports both light and
+	// dark terminals
+	//
+	// [logging]: https://github.com/charmbracelet/log
+	Logging = &log.Styles{
+		Timestamp: lipgloss.NewStyle(),
+		Caller:    lipgloss.NewStyle().Faint(true),
+		Prefix:    lipgloss.NewStyle().Bold(true).Faint(true),
+		Message:   lipgloss.NewStyle().MarginRight(2),
+		Key: lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{
+				Light: string(S400),
+				Dark:  string(S100),
+			}),
+		Value:     lipgloss.NewStyle(),
+		Separator: lipgloss.NewStyle().Faint(true),
+		Levels: map[log.Level]lipgloss.Style{
+			log.DebugLevel: lipgloss.NewStyle().
+				SetString(Tick).
+				Bold(true).
+				MaxWidth(2),
+			log.InfoLevel: lipgloss.NewStyle().
+				SetString(Tick).
+				Bold(true).
+				MaxWidth(2),
+			log.WarnLevel: lipgloss.NewStyle().
+				SetString(Bang).
+				Bold(true).
+				MaxWidth(2),
+			log.ErrorLevel: lipgloss.NewStyle().
+				SetString(Cross).
+				Bold(true).
+				MaxWidth(2),
+			log.FatalLevel: lipgloss.NewStyle().
+				SetString(Cross).
+				Bold(true).
+				MaxWidth(2),
+		},
+		Keys: map[string]lipgloss.Style{
+			"err": lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
+				Light: string(Red900),
+				Dark:  string(Red700),
+			}),
+			"error": lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{
+				Light: string(Red900),
+				Dark:  string(Red700),
+			}),
+		},
+		Values: map[string]lipgloss.Style{},
+	}
+)
---
+++ table.go
@@ -0,0 +1,284 @@
+package theme
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+// TableBorder defines a series of characters that are used when rendering the
+// border of a table
+type TableBorder struct {
+	Bottom      string
+	BottomJoin  string
+	BottomLeft  string
+	BottomRight string
+	Middle      string
+	MiddleJoin  string
+	MiddleLeft  string
+	MiddleRight string
+	MiddleTop   string
+	Top         string
+	TopJoin     string
+	TopLeft     string
+	TopRight    string
+	Vertical    string
+}
+
+var (
+	bdr = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{
+			Light: string(S400),
+			Dark:  string(S200),
+		})
+
+	cell = lipgloss.NewStyle().Padding(0, 1)
+)
+
+// Table supports the rendering of tabular data within a terminal
+type Table struct {
+	border     TableBorder
+	rowHeights []int
+	colWidths  []int
+	data       [][]string
+	top        string
+	middle     string
+	bottom     string
+	dividers   bool
+	collapsed  bool
+	alignments [][]lipgloss.Position
+}
+
+// NewTable creates a table that will dynamically size around its provided
+// data. By default the table will be rendered without any border
+//
+//	data := [][]string{
+//		{"City", "Avg. Rainfall", "Avg. Temp"},
+//		{"Barclona", "640 mm", "21.2 °C"},
+//		{"London", "585 mm", "11 °C"},
+//	}
+//	theme.NewTable(data)
+func NewTable(data [][]string) *Table {
+	t := &Table{
+		border:     NoBorder,
+		rowHeights: []int{},
+		colWidths:  []int{},
+		data:       data,
+		dividers:   true,
+		collapsed:  false,
+	}
+
+	t.resetAlignments()
+	t.maxDimensions()
+	t.resetDividers()
+	return t
+}
+
+func (t *Table) resetAlignments() {
+	if len(t.data) == 0 || len(t.data[0]) == 0 {
+		return
+	}
+
+	t.alignments = make([][]lipgloss.Position, len(t.data[0]))
+	for i := range t.data[0] {
+		t.alignments[i] = make([]lipgloss.Position, 2)
+		t.alignments[i][0] = lipgloss.Left
+		t.alignments[i][1] = lipgloss.Top
+	}
+}
+
+func (t *Table) maxDimensions() {
+	if len(t.data) == 0 || len(t.data[0]) == 0 {
+		return
+	}
+
+	cellStyle := cell
+	if t.collapsed {
+		cellStyle = cellStyle.UnsetPadding()
+	}
+
+	t.rowHeights = make([]int, len(t.data))
+	t.colWidths = make([]int, len(t.data[0]))
+
+	for i, row := range t.data {
+		for j, c := range row {
+			w, h := lipgloss.Size(cellStyle.Render(c))
+			t.colWidths[j] = max(t.colWidths[j], w)
+			t.rowHeights[i] = max(t.rowHeights[i], h)
+		}
+	}
+}
+
+func (t *Table) resetDividers() {
+	t.top = divider(
+		t.border.TopLeft,
+		t.border.Top,
+		t.border.TopJoin,
+		t.border.TopRight,
+		t.colWidths...,
+	)
+
+	t.middle = divider(
+		t.border.MiddleLeft,
+		t.border.Middle,
+		t.border.MiddleJoin,
+		t.border.MiddleRight,
+		t.colWidths...,
+	)
+
+	t.bottom = divider(
+		t.border.BottomLeft,
+		t.border.Bottom,
+		t.border.BottomJoin,
+		t.border.BottomRight,
+		t.colWidths...,
+	)
+}
+
+func divider(left, sep, join, right string, cellW ...int) string {
+	var d strings.Builder
+	for _, mw := range cellW {
+		d.WriteString(strings.Repeat(sep, mw) + join)
+	}
+	mid := strings.TrimRight(d.String(), join)
+
+	return lipgloss.JoinHorizontal(
+		lipgloss.Left,
+		bdr.Render(left),
+		bdr.Render(mid),
+		bdr.Render(right),
+	)
+}
+
+func verticalDivider(str string, h int) string {
+	d := strings.Repeat(str+"\n", h)
+	return bdr.Render(strings.TrimRight(d, "\n"))
+}
+
+// Border sets the table border
+func (t *Table) Border(border TableBorder) *Table {
+	t.border = border
+	t.resetDividers()
+	return t
+}
+
+// Widths sets the maximum widths of each colum within the table. If only
+// one argument is provided all columns will be fixed to the same width.
+// If more than one argument is provided, each corresponding columns width
+// will be fixed in turn.
+func (t *Table) Widths(w ...int) *Table {
+	t.resetMaxWidths(w...)
+	t.resetDividers()
+	return t
+}
+
+func (t *Table) resetMaxWidths(w ...int) {
+	if len(w) == 0 {
+		return
+	}
+
+	if len(w) == 1 {
+		for i := 0; i < len(t.colWidths); i++ {
+			t.colWidths[i] = w[0]
+		}
+		return
+	}
+
+	cols := min(len(t.colWidths), len(w))
+	for i := 0; i < cols; i++ {
+		t.colWidths[i] = w[i]
+	}
+}
+
+// HorizontalAlignments is a shorthand method for setting the horizontal alignment of
+// columns within the table. All columns will adopt the same alignment when only
+// one argument is set. Each column will adopt its own alignment if more than one
+// argument is set. If the number of alignments is less than the number of columns,
+// then those columns remain untouched
+func (t *Table) HorizontalAlignments(p ...lipgloss.Position) *Table {
+	t.setAlignments(0, p...)
+	return t
+}
+
+// VerticalAlignments is a shorthand method for setting the vertical alignment of
+// columns within the table. All columns will adopt the same alignment when only
+// one argument is set. Each column will adopt its own alignment if more than one
+// argument is set. If the number of alignments is less than the number of columns,
+// then those columns remain untouched
+func (t *Table) VerticalAlignments(p ...lipgloss.Position) *Table {
+	t.setAlignments(1, p...)
+	return t
+}
+
+func (t *Table) setAlignments(pos int, p ...lipgloss.Position) {
+	if len(p) == 0 {
+		return
+	}
+
+	if len(p) == 1 {
+		for i := 0; i < len(t.colWidths); i++ {
+			t.alignments[i][pos] = p[0]
+		}
+		return
+	}
+
+	cols := min(len(t.colWidths), len(p))
+	for i := 0; i < cols; i++ {
+		t.alignments[i][pos] = p[i]
+	}
+}
+
+// Dividers controls whether a row divider should be rendered
+// between all table rows
+func (t *Table) Dividers(on bool) *Table {
+	t.dividers = on
+	return t
+}
+
+// Collapsed controls whether all internal padding within the
+// table should be removed
+func (t *Table) Collapsed(on bool) *Table {
+	t.collapsed = on
+	t.maxDimensions()
+	return t
+}
+
+// String renders the table as a formatted string
+func (t *Table) String() string {
+	if len(t.data) == 0 {
+		return ""
+	}
+
+	cellStyle := cell
+	if t.collapsed {
+		cellStyle = cellStyle.UnsetPadding()
+	}
+
+	var tblRows []string
+	for i, row := range t.data {
+		var tblRow []string
+
+		rowH := t.rowHeights[i]
+		vertJoin := verticalDivider(t.border.Vertical, rowH)
+
+		for j, col := range row {
+			cell := cellStyle.Width(t.colWidths[j]).
+				Align(t.alignments[j]...).
+				Render(col)
+
+			tblRow = append(tblRow, vertJoin, cell)
+		}
+		tblRow = append(tblRow, vertJoin)
+		tblRows = append(tblRows, lipgloss.JoinHorizontal(lipgloss.Left, tblRow...))
+		if t.dividers && i < len(t.data)-1 {
+			tblRows = append(tblRows, t.middle)
+		}
+	}
+
+	return lipgloss.JoinVertical(
+		lipgloss.Top,
+		t.top,
+		strings.Join(tblRows, "\n"),
+		t.bottom,
+	)
+}
